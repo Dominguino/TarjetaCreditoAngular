f8521bd3eb1468dc40029b1dff8e67b34ef8a90c
Domin <domingoandresramos@gmail.com>
2023-02-07 13:40:36 +0100

    añadido API backend para el CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+import { ToastrModule } from 'ngx-toastr';
+
 import { AppComponent } from './app.component';
 import { TarjetaCreditoComponent } from './components/tarjeta-credito/tarjeta-credito.component';
+
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -13,7 +17,10 @@ import { TarjetaCreditoComponent } from './components/tarjeta-credito/tarjeta-cr
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(), 
+    HttpClientModule,
     ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { TarjetaService } from 'src/app/service/tarjeta.service';
+
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -16,21 +19,53 @@ export class TarjetaCreditoComponent {
   ]; 
   form: FormGroup; 
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder, 
+    private toastr: ToastrService,
+    private _tarjetaService: TarjetaService){
+    
     this.form = this.fb.group({
-      titular: [''], 
-      numeroTarjeta: [''],
-      fechaExpiracion: [''], 
-      cvv: ['']
+      titular: ['', Validators.required], 
+      numeroTarjeta: ['', [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
+      fechaExpiracion: ['',[Validators.required, Validators.maxLength(5), Validators.minLength(5)]], 
+      cvv: ['', [Validators.required, Validators.maxLength(3), Validators.minLength(3)]]
     })
    
   }
 
-  ngOnInit(): void{}
+  ngOnInit(): void{
+    this.obtenerTarjetas(); 
+  }
 
+
+  obtenerTarjetas(){
+    this._tarjetaService.getListTarjetas().subscribe(data => {
+      console.log(data); 
+    }, error=> {
+      console.log(error); 
+    })
+  }
 
   agregarTarjeta(){
     console.log(this.form)
+
+    const tarjeta: any = {
+      titular: this.form.get('titular')?.value, 
+      numeroTarjeta: this.form.get('numeroTarjeta')?.value, 
+      fechaExpiracion: this.form.get('fechaExpiracion')?.value, 
+      cvv: this.form.get('cvv')?.value, 
+
+    }
+    this.listTarjetas.push(tarjeta); 
+
+    this.toastr.success('La tarjeta fue registrada con éxito', 'Tarjeta Registrada');    
+    this.form.reset(); 
+
+  }
+
+  eliminarTarjeta(index: number){
+    this.listTarjetas.splice(index, 1); 
+    this.toastr.error('La tarjeta fue eliminada con exito', 'La tarjeta fue eliminada');
+
   }
 
   
